0c87a9096c49ce6e3db1f989ff29114472817279
Thomas Vendetta <thomas@thomasvendetta.com>
2014-12-16 20:35:35 -0500

    Doh, think I figured it out

--- tmbs/http_push_receiver.go
+++ tmbs/http_push_receiver.go
@@ -1,54 +1,30 @@
 package tmbs
 
 import (
-	"encoding/json"
 	"fmt"
-	"io"
-	"io/ioutil"
 	"net/http"
 )
 
 func RenderPushNotification(res http.ResponseWriter, req *http.Request) {
 	res = setHeader(res)
 
-	var js interface{}
-
-	decoder := json.NewDecoder(req.Body)
-	err := decoder.Decode(&js)
+	err := req.ParseForm()
 	if err != nil {
-		fmt.Println("ERROR DECODING", err)
-
-		var body []byte
-		body, err := ioutil.ReadAll(req.Body)
-
-		sample := body
-
-		fmt.Println("Printf with %x:")
-		fmt.Printf("%x\n", sample)
-
-		fmt.Println("Printf with % x:")
-		fmt.Printf("% x\n", sample)
-
-		fmt.Println("Printf with %q:")
-		fmt.Printf("%q\n", sample)
-
-		fmt.Println("Printf with %+q:")
-		fmt.Printf("%+q\n", sample)
-
-		// This line prints out the output above
-		fmt.Println(string(body))
-		exitIfError(err, "What the heck")
-	}
-	jsonbytes, err := json.MarshalIndent(js, " ", " ")
-
-	if err != nil {
-		fmt.Println("Received invalid json?")
-	} else {
-		err = ioutil.WriteFile("example.json", jsonbytes, 0644)
-		exitIfError(err, "Couldn't save file.")
+		fmt.Println("Error parsing form")
 	}
 
-	io.WriteString(res, string(jsonbytes))
+	fmt.Println(req.PostForm)
+
+	//	jsonbytes, err := json.MarshalIndent(js, " ", " ")
+
+	//	if err != nil {
+	//		fmt.Println("Received invalid json?")
+	//	} else {
+	//		err = ioutil.WriteFile("example.json", jsonbytes, 0644)
+	//		exitIfError(err, "Couldn't save file.")
+	//	}
+
+	//	io.WriteString(res, string(jsonbytes))
 
 	// Print what we need
 	//	fmt.Println(string(body))
